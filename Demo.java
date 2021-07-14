3bb66b7b88986ea8ee00a09ef24fb4aa01a02f71
felixvl <digitaleservice@gmail.com>
2021-07-14 11:19:25 +0200

    SC

--- Demo.java
+++ Demo.java
@@ -1,25 +1,35 @@
-class Demo{   // SA
+class Demo{   // SC
 	 static public final void main(String[] args){
 		Iets i = new Nope();
 		i.anders();
+		System.out.println(i.a);
 		System.out.println("bye");
 	}
 }
 
 abstract class Iets{
+	int a = 3;
 	void anders(){
 		System.out.println("anders");
 	}
+	void worden();
 	
 }
 
 class Dus extends Iets{
+	int a = 4;
 	void anders(){
 		System.out.println("dus anders");
 		return;
 	}
+	void worden(){
+		System.out.println("Dus worden");
+	}
 }
 
 class Nope extends Iets{
-
+	int a = 5;
+	void worden(){
+		System.out.println("Nope worden");
+	}
 }
