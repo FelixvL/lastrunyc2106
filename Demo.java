ed16e9dfa75f41a8c3aad91ef6e45c75c17c37ab
felixvl <digitaleservice@gmail.com>
2021-07-14 11:00:25 +0200

    RE

--- Demo.java
+++ Demo.java
@@ -1,16 +1,14 @@
-class Demo{   // RC
+class Demo{   // RE
 	 static public final void main(String[] args){
 		System.out.println("start");
-		H h = new H();
-		D d = new D();
-		h = d;
-		h.lopen();
+		W w = new W();
+		w.lopen().lopen(5);
 		System.out.println("eind");
 	}
 }
 class W{
-	void lopen(){
-		System.out.println("lopen in W");
+	H lopen(){
+		return new D();
 	}
 }
 
