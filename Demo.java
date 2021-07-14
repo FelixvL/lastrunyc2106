069dc388136066627923daf233b05c4f8e570695
felixvl <digitaleservice@gmail.com>
2021-07-14 10:51:46 +0200

    RY

--- Demo.java
+++ Demo.java
@@ -1,9 +1,9 @@
-class Demo{   // RX
+class Demo{   // RY
 	 static public final void main(String[] args){
 		System.out.println("start");
 		H h = new H();
 		D d = new D();
-		d.lopen();
+		h.lopen();
 		System.out.println("eind");
 	}
 }
@@ -11,12 +11,12 @@ class Demo{   // RX
 
 class H{
 	void lopen(int a){
-		System.out.println("lopen in U");
+		System.out.println("lopen in T");
 	}
 }
 
 class D extends H{
 	void lopen(){
-		System.out.println("lopen in UR");
+		System.out.println("lopen in TR");
 	}
 }
