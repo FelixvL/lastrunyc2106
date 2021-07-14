a6949f0c1ec3ed2509f48505a00f594d8673e4d4
felixvl <digitaleservice@gmail.com>
2021-07-14 10:37:41 +0200

    RT

--- Demo.java
+++ Demo.java
@@ -1,4 +1,14 @@
 class Demo{   // RT
+	final static void main(String[] args){
+		System.out.println("start");
+		Hond d = new Dier();
+		System.out.println("eind");
+	}
+}
 
+class Dier extends Hond{
+
+}
+class Hond{
 
 }
