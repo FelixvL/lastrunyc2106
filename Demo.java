30c069f7f8c3e20a41643ae3b7306bcad311fcc5
felixvl <digitaleservice@gmail.com>
2021-07-14 11:09:26 +0200

    RG

--- Demo.java
+++ Demo.java
@@ -1,19 +1,21 @@
-class Demo{   // RF
+class Demo{   // RG
 	 static public final void main(String[] args){
 		System.out.println("start");
 		W w = new W();
+		H h = new D();
+		h.lopen(5);
 		w.lopen().lopen(5);
 		System.out.println("eind");
 	}
 }
 class W{
-	H lopen(){
+	D lopen(){
 		return new D();
 	}
 }
 
 class H extends W{
-	void lopen(int a){
+	void huppakee(int a){
 		System.out.println("lopen in Z");
 	}
 }
