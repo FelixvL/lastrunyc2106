553641987418ac3c32434b5a40110f7edefbb6c6
felixvl <digitaleservice@gmail.com>
2021-07-19 11:27:37 +0200

    DK

--- Demo.java
+++ Demo.java
@@ -1,25 +1,26 @@
-class Demo{   // DJ
+class Demo{   // DK
 	 static public final void main(String[] args){
 		System.out.println("start");
 		RRR rrr = new RRR();
-		AAA a = rrr.gaan(new FFF());
+		rrr.gaan(new AAA());		
+
 		System.out.println("einde");
 
 	}
 }
 
-class RRR{
-	DDD gaan(AAA a){
+class RRR{    
+	DDD gaan(AAA a){          
 		return new FFF();
 	}
 }
 
-class DDD implements AAA{
+class DDD implements AAA{     
 
 }
-class FFF extends DDD{
+class FFF extends DDD{     
 
 }
-interface AAA{
+interface AAA{     
 
 }
