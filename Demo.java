9a16aa5a9082200b4a22421a410db03406f1c8d4
felixvl <digitaleservice@gmail.com>
2021-07-19 11:35:37 +0200

    DM

--- Demo.java
+++ Demo.java
@@ -1,29 +1,37 @@
-class Demo{   // DL
+class Demo{   // DM
 	 static public final void main(String[] args){
 		System.out.println("start");
 		RRR rrr = new RRR();
 		rrr.gaan(new QQQ().iets());		
-
 		System.out.println("einde");
-
 	}
 }
-
 class RRR{    
-	DDD gaan(AAA a){ 
-		System.out.println("b");         
+	DDD gaan(DDD a){ 
+		System.out.println("b"); 
+		a.checken();
+		System.out.println(a.check);        
 		return new FFF();
 	}
 }
-class DDD implements AAA{     
+class DDD implements AAA{  
+	int check = 8; 
+	public void checken(){
+		System.out.println("in d");
+	}  
 }
-class FFF extends DDD{     
+class FFF extends DDD{  
+	int check = 9;
+	public void checken(){
+		System.out.println("in f");
+	}    
 }
 class QQQ{
-	FFF iets(){
+	AAA iets(){
 		System.out.println("a");
-		return FFF();	
+		return new FFF();	
 	}
 }
-interface AAA{     
+interface AAA{  
+	void checken();   
 }
