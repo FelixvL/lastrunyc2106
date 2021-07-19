8fef6221c61ad6d782e92e0bbb2bf96081d5f1fd
felixvl <digitaleservice@gmail.com>
2021-07-19 09:58:18 +0200

    CF

--- Demo.java
+++ Demo.java
@@ -1,8 +1,8 @@
-class Demo{   // CC
+class Demo{   // CF
 	 static public final void main(String[] args){
 		System.out.println("start");
 		Repairable r = new Car();
-		Vehicle v = (Repairable) r;
+		Vehicle v = (Car) r;
 		v.repair();
 		System.out.println("end");
 
@@ -11,17 +11,19 @@ class Demo{   // CC
 
 
 
-class Car extends Vehicle {
+class Car extends Vehicle {    // Paard
 	public void repair(){
 		System.out.println("repair in Car");
 	}
 }
-class Vehicle implements Repairable{
+class Vehicle implements Repairable{   // Dier
 	public void repair(){
 		System.out.println("repair in Vehicle");
 	}
 }
 
-interface Repairable{
+interface Repairable{      // Categoriseerbaar
 	void repair();
 }
+
+// Pokemonkaarten      // Categoriseerbaar
