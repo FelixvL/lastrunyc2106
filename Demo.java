ba039daf3647ebf70fab2c70b8ad0346f691e4a9
felixvl <digitaleservice@gmail.com>
2021-07-19 10:39:03 +0200

    CN

--- Demo.java
+++ Demo.java
@@ -1,27 +1,26 @@
-class Demo{   // CM
+class Demo{   // CN
 	 static public final void main(String[] args){
-		Dier dier = new Shetlander();
-		System.out.println(dier.snelheid());
-		Dier dier1 = new Hond();
-		System.out.println(dier1.snelheid());
+		Voortbewegen v = new Shetlander();
+		Paard p = (Paard) v;
+		System.out.println(p.snelheid());
 	}
 }
 abstract class Dier implements Voortbewegen{
 }
 class Paard extends Dier{
-	int snelheid(){
+	public int snelheid(){
 		System.out.println("snelheid in paard");
 		return 7;
 	}
 }
 class Shetlander extends Paard{
-	int snelheid(){
+	public int snelheid(){
 		System.out.println("snelheid in shetland");
 		return 5;
 	}
 }
 class Hond extends Dier{
-	int snelheid(){
+	public int snelheid(){
 		System.out.println("snelheid in hond");
 		return 3;
 	}
