669f8ac9cb7ffd284a307a2ab80859735a0dcee5
felixvl <digitaleservice@gmail.com>
2021-07-19 09:35:24 +0200

    ZB

--- Demo.java
+++ Demo.java
@@ -1,20 +1,23 @@
-class Demo{   // ZZ
-	 static public final void main(String[] args) throws Exception{
+class Demo{   // ZB
+	 static public final void main(String[] args){
 		System.out.println("start");
 		FFproberen ff = new Goedproberen();
-		ff.wandelen();
+		ff.go().wandelen();
 		System.out.println("einde");
 	}
 }
 
 class FFproberen{
-	void wandelen() throws Exception{
+	void wandelen(){
 		System.out.println("a");
+	}
+	Goedproberen go(){
+		return new FFproberen();
 	}
 }
 
 class Goedproberen extends FFproberen{
-	void wandelen() throws Exception{
+	void wandelen(){
 		System.out.println("b");
 	}
 }
